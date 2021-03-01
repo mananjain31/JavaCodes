512af1cebcb7227038ac976bff6bc1f2ddf1eaf1
Manan Jain <mananjain31jan@gmail.com>
2021-03-01 15:35:03 +0530

    Fetch all data from the table in cmd

---
+++ Database/FetchAllData.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+class FetchAllData
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			String db_url = "jdbc:mysql://localhost:3306/manandb?useSSL=false";
+			String db_uname = "manan";
+			String db_upass = "Manan+31";
+			Connection con = DriverManager.getConnection(db_url, db_uname, db_upass);
+
+			Statement st = con.createStatement();
+
+			String q = "select * from login";
+			//creating object of ResultSet to fetch the table "login"
+			ResultSet rs = st.executeQuery(q);
+			//by default rs points to the top so to make it point to the first row we do rs.next()
+			while(rs.next())
+			{
+				System.out.println(rs.getString(1)+"\t"+rs.getString(2));
+			}
+			con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
